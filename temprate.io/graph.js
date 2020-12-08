072f62194e7c6e3af774fe7307c1edee2666bfc6
A R SUMUKHA <sumuar1@gmail.com>
2020-12-08 21:34:49 +0530

    working prototype phase 1

--- temprate.io/graph.js
+++ temprate.io/graph.js
@@ -1,62 +1,74 @@
+var yvalue;
+var xlabel;
 async function func(){
-            var x=document.getElementById('x-axis').value;
-            var y=document.getElementById('y-axis').value;
-            getdata();
-            const xlabel=[];
-            const value=[];
-            const xyval=[];
-            const constant=[];
+            //from text
+            var x=document.getElementById('x').value;
+            var y=document.getElementById('y').value;
+            var y1=document.getElementById('y1').value;
+            var y2=document.getElementById('y2').value;
+            var fname=document.getElementById('filename').value;
+            fname=fname+'.csv';
+            yvalue=[];
+            xlabel=[];
+            //calling get data for parsing the func
+            await getdata();
+            //var for pushing data set 
+            
             var ctx = document.getElementById('Charts').getContext('2d');
+
+            var arr=[] ;
+            
+            for(let i=0;i<3;i++){
+                console.log(yvalue[i]);
+                var obj={
+                    fill:false,
+                    label: "gloabal avg temperature" ,
+                    data: yvalue[i],
+                    backgroundColor:['rgba(255, 0, 0, 0.5)'],
+                    borderColor:['rgba(255,25,25,0.6)'],
+                    borderWidth: 2
+                    };
+                    arr.push(obj)
+            }
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: xlabel,
-        datasets: [
-            {
-            fill:false,
-            label: "gloabal avg temperature" ,
-            data: value,
-            backgroundColor:['rgba(255, 0, 0, 0.5)'],
-            borderColor:['rgba(255,25,25,0.6)'],
-            borderWidth: 2
-            },
-            {
-            fill:false,
-            label: "gloabal avg temperature" ,
-            data: xyval,
-            backgroundColor:['rgba(0, 0, 255, 0.3)'],
-            borderColor:['rgba(0,25,255,0.4)'],
-            borderWidth: 2
-            },
-            {
-            fill:false,
-            label: "global avg constant" ,
-            data: constant,
-            backgroundColor:['rgba(0, 0, 0, 0.3)'],
-            borderColor:['rgba(0,0,0,1)'],
-            borderWidth: 3
-            }
-            ]
+        datasets:arr
     }
 });
             async function getdata(){
-                const res=await fetch('GLB.Ts+dSST.csv');
+                const res=await fetch(fname);
                 const ts=await res.text();
                 var table=ts.split('\n');
                 const header=table[0];
+                const v1=[]
+                const v2=[]
+                const v3=[]
+
                 table=ts.split('\n').slice(1);
                 table.forEach(row=>{
                     const col=row.split(",");
-                    const year=col[x];
-                    var temp=col[y];
-                    temp=parseFloat(temp)+15;
-                    var temp1=col[2];
-                    temp1=parseFloat(temp1)+15;
-                    xyval.push(temp1);
-                    xlabel.push(year);
-                    value.push(temp);
-                    constant.push(15);
-                    console.log(year,temp);
+                    const val=parseFloat(col[y])+15;
+                    const val1=parseFloat(col[y1])+15;
+                    const val2=parseFloat(col[y2])+15;
+                    const x1=col[x];
+                    xlabel.push(x1);
+                    v1.push(val);
+                    v2.push(val1);
+                    v3.push(val2);
+                    
                 });
+                yvalue[0]=v1;
+                yvalue[1]=v2;
+                yvalue[2]=v3;
+                console.log(yvalue[0]);
             }
+            
+          console.log(yvalue[0]);  
+}
+
+
+async function reset(){
+    window.location.reload();
 }
